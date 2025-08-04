dbb688fcd3e6ebcc3ab1863d90222a1421698278
el_the_forgotten <eladar8448@gmail.com>
2025-08-04 18:36:45 -0400

    Week 0: Basiccalculator with math operations

---
+++ week0/calculator.py
@@ -0,0 +1,7 @@
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+
+print(f"{x} + {y} = {x + y}")
+print(f"{x} - {y} = {x - y}")
+print(f"{x} * {y} = {x * y}")
+print(f"{x} / {y} = {float(x / y)})")
