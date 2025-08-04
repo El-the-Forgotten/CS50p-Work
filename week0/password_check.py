c22e0cf781f3563efcf931fca1dbd4a019cfb479
el_the_forgotten <eladar8448@gmail.com>
2025-08-04 19:03:21 -0400

    Week 0: Password check with multi-condition login

---
+++ week0/password_check.py
@@ -0,0 +1,10 @@
+varname = "Corvell"
+varpass = "22816LondonDerry"
+
+username = input("Username: ")
+password = input("Password: ")
+
+if username == varname and password == varpass:
+    print("Access granted")
+else:
+    print("Access Denied")
